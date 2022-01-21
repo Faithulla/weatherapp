ab93bbe2a88edcb2ed26efc8ddf1dc84af970344
Faithulla Shakirov <fayzullahsakirov01@gmail.com>
2022-01-21 23:14:26 +0500

    initializing app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((item) => {
+        return <div>
+        
+        {item.name}
+        
+        </div>;
+      })}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/weather-shablon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WeatherShablon = () => {
+  return (
+    <div>
+      <div className="weather-shablon">
+        <div className="header"></div>
+        <div className="body"></div>
+        <div className="footer"></div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherShablon;
